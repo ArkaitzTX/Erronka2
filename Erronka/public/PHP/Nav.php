38d5887854f369dd931cf11d7d22010fc1f9f407
Mikel <mikelhortega@gmail.com>
2022-12-19 10:50:57 +0100

    Nav y Footer

--- Erronka/public/PHP/Nav.php
+++ Erronka/public/PHP/Nav.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="Header,Footer.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('CSS/Header,Footer.css')}}" rel="stylesheet" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
@@ -21,7 +21,7 @@
                         aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <img class="navbar-brand" src="Logo.png" height="36">
+                    <img class="navbar-brand" src="{{asset('IMG/Logo.png')}}" height="36">
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                             <li class="nav-item active">
